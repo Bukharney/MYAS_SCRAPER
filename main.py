789a58e738df86bd84b6cda7bcd6747ed950a12f
Jirapat <bukharney@gmail.com>
2024-02-04 22:51:14 +0700

    refactor & logging

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from playwright.sync_api import sync_playwright
-from bs4 import BeautifulSoup
-
-# get env variable
 from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+import logging
+import json
 import os
+
 
 load_dotenv(".env")
 
@@ -11,7 +12,13 @@ username = os.getenv("USERNAME")
 password = os.getenv("PASSWORD")
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
+    logging.basicConfig(
+        format="[%(levelname)s] %(message)s",
+        level=logging.INFO,
+        datefmt="%d-%m-%y %H:%M:%S",
+    )
+    logging.info("Logging in...")
+    browser = p.chromium.launch()
     page = browser.new_page()
     page.goto(
         "https://login1.leb2.org/login?app_id=1&redirect_uri=https%3A%2F%2Fapp.leb2.org%2Flogin"
@@ -22,53 +29,52 @@ with sync_playwright() as p:
     page.wait_for_load_state("load")
     soup = BeautifulSoup(page.inner_html("#classListMain"), "html.parser")
 
+    logging.info("Getting classes...")
     class_list = []
-    # get name arrtibute in div tag
     for div in soup.findAll("div", {"class": "class-card"}):
-        # card-511287 cut card- and get the number
         class_list.append(div.attrs["name"][5:])
 
+    logging.info("Getting assignments...")
     assignments_list = []
     for class_id in class_list:
         page.goto(f"https://app.leb2.org/class/{class_id}/activity")
         page.wait_for_load_state("load")
-        # get text in p tag arrtibute name=code
-        class_name = page.inner_text("p[name=code]")
+        class_name = (
+            page.inner_text("p[name=code]") + " : " + page.inner_text("p[name=name_ln]")
+        )
         soup = BeautifulSoup(page.inner_html("#table"), "html.parser")
-        # get span tag in td tag with attribute data-label=Submissions
 
         assignments = {
             "class_name": class_name,
             "assignments": [],
         }
 
+        # find all the rows in the table
         for tr in soup.findAll("tr"):
             assignment = {}
-            if tr.find("a", {"class": "assessment__title-link"}):
-                name = tr.find("a", {"class": "assessment__title-link"}).text
-                assignment["name"] = name
-            if tr.find("td", {"data-label": "Submissions"}):
+            if tr.find("a"):
+                assignment["name"] = tr.find(
+                    "a", {"class": "assessment__title-link"}
+                ).text
+            if tr.find("td"):
                 assignment["submission"] = tr.find(
                     "td", {"data-label": "Submissions"}
                 ).text
-            if tr.find("td", {"data-label": "Due Date"}):
-                # get text in td tag
-                td = tr.find("td", {"data-label": "Due Date"})
-                span = td.find("span")
-                assignment["due_date"] = span.attrs["title"]
+                assignment["due_date"] = (
+                    tr.find("td", {"data-label": "Due Date"})
+                    .find("span")
+                    .attrs["title"]
+                )
+
             if assignment:
                 assignments["assignments"].append(assignment)
 
         assignments_list.append(assignments)
 
-    print(assignments_list)
-
-    # write to json file
-    import json
-
+    logging.info("Writing to file...")
     with open("assignments.json", "w") as f:
-        # make it pretty
-        # uft-8 for thai language
         f.write(json.dumps(assignments_list, indent=4, ensure_ascii=False))
 
     browser.close()
+
+    logging.info("Done!")
